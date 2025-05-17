52ad6749c0d4cf4cd4b2fce46c579e46ccb77b94
tetiana-martseniuk <tatyanamartseniuk@gmail.com>
2025-05-17 14:15:57 +0100

    Updated

--- Параноя.js
+++ Параноя.js
@@ -16,19 +16,11 @@ var arr = [
         email:"andrii@mail.ru" // Нам такі не підходять
     },
 ];
- 
-let regex = /^[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)?@(gmail\.com|yahoo\.com)$/;
-let results = arr.filter(element => {
-    if (!element.email) return false;
-    let cleanEmail = element.email.replace(/[<>]/g, '');
-    return regex.test(cleanEmail);
-  });
-for (let i = 0; i < arr.length; i++) {
-    let email = arr[i].email.replace(/[<>]/g, '');
-    if (regex.test(email)) {
-        console.log("Валідний імейл: " + email);
-    } else {
-        console.log("Невалідний імейл: " + email);
-    }
- };
 
+let regex = /<?\w+(\.\w+)?@(gmail\.com|yahoo\.com)>?/;
+for (let i = 0; i < arr.length; i++) {
+    let email = arr[i].email;
+    if (regex.test(email)) {
+        console.log("Trusted email: " + email);
+    }; 
+ };
