7a9c0c1f2e67f8fd71d8063cdb0762fdd34438b5
tetiana-martseniuk <tatyanamartseniuk@gmail.com>
2025-06-26 19:29:26 +0300

    Some files were deleted

--- Without_A.js
+++
@@ -1,4 +0,0 @@
-let words = ["Wonderful", "Joyful", "Happiness", "Time", "Task", "Apple"]; 
-let regex = /^[^Aa]{6,}$/;
-let results = words.filter(words => regex.test(words)); 
-console.log(results); 
